75989fbd764b021ef9cd6be87e487ba29de93163
toskrip <tomas@skripcak.net>
2023-09-06 12:30:16 +0200

    fix: typo in theme name

--- conf.py
+++ conf.py
@@ -92,7 +92,7 @@ if not on_rtd:
   html_theme = 'sphinx_rtd_theme'
   html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 else:
-  html_theme = 'spinx_rtd_theme'
+  html_theme = 'sphinx_rtd_theme'
 
 numfig = True
 
